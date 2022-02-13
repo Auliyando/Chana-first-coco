7b37a88a0d1eb93403bba4abfc0083886649323a
Auliyando <muhammad.auliya.rahman-2020@stmm.unair.ac.id>
2022-02-13 08:51:45 +0700

    add preloader

--- assets/js/Lightbox-Gallery.js
+++ assets/js/Lightbox-Gallery.js
@@ -1,3 +1,11 @@
 if (document.querySelectorAll('[data-bss-baguettebox]').length > 0) {
    baguetteBox.run('[data-bss-baguettebox]', { animation: 'slideIn' });
 }
+function popUp() {
+    var loader = document.getElementById('preloader');
+    loader.style.visibility = "visible";
+  setTimeout(function () {
+    document.getElementById('preloader').style.display = "none";
+  }, 5000);   
+});
+popUp();
