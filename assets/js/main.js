aee5b7d881cfe390aa6e54c0d8a3531f69251bb1
Auliyando <muhammad.auliya.rahman-2020@stmm.unair.ac.id>
2022-03-28 07:56:19 +0700

    Add Preloader and Fix sitemap.xml

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 var loader = document.getElementById("preloader");
-ind = document.getElementById("Carousel")
-ind.addEventListener("load",function(){
+ind = document.getElementById("Beres");
+window.addEventListener("load",function(){
     loader.style.display = "none";
 })
